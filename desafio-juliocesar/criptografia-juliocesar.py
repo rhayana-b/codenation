6ac4c084acad8a271ab016f314d59a9982879e6a
rhayana-b <rhayanabarbosa@gmail.com>
2020-04-30 16:06:38 -0300

    arrumando comentários

--- desafio-juliocesar/criptografia-juliocesar.py
+++ desafio-juliocesar/criptografia-juliocesar.py
@@ -3,7 +3,8 @@ import json
 import string
 import hashlib
 
-my_token = '8d73d4a4bdacc60186b06f59b93eedd87780a7c2'
+#My Token from Codenation
+my_token = ''
 
 # Request HTTP 
 r = requests.get('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token={token}'.format(token=my_token))
@@ -13,11 +14,14 @@ res_json = r.json()
 with open('answer.json', 'w') as json_file:
     json.dump(res_json, json_file, indent=2)
 
+# Get the value from keys from the JSON
 cifrado = res_json['cifrado']
 numero_casas = res_json['numero_casas']
 
-alphabet = string.ascii_lowercase #alphabet from a to z
+# Use the Alphabet from a to z
+alphabet = string.ascii_lowercase 
 
+#Create Function to decrypt the message
 def decrypt(encrypted, rotation):
     result = ''
     for letter in encrypted:
@@ -29,8 +33,10 @@ def decrypt(encrypted, rotation):
             result += letter
     return result
 
+# Use the Function to get the result 
 decifrado = decrypt(cifrado, numero_casas)
 
+# Update the .json file with new data for a Key
 with open('answer.json', 'r') as json_file:
     new_data = json.load(json_file)
     new_data['decifrado'] = decifrado
@@ -38,9 +44,10 @@ with open('answer.json', 'r') as json_file:
 with open('answer.json', 'w') as json_file:
     json_file.write(json.dumps(new_data, indent=2))
 
-
+# 
 resumo_criptografico = hashlib.sha1(str(decifrado).encode('utf-8')).hexdigest()
 
+# Update the .json file again with new data for another Key
 with open('answer.json', 'r') as json_file:
     new_data = json.load(json_file)
     new_data['resumo_criptografico'] = resumo_criptografico
@@ -48,6 +55,7 @@ with open('answer.json', 'r') as json_file:
 with open('answer.json', 'w') as json_file:
     json_file.write(json.dumps(new_data, indent=2))
 
+# Post the answer to Codenation as a multipart/form-data
 r = requests.post(
     'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token={token}'.format(token=my_token), 
     files={'answer': open('answer.json', 'rb')}
